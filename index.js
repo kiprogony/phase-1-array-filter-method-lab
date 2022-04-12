8bca42f1eecfab7c09913ecf2878ee0f6d0a5aa8
Daryl Rogony <darylrogony@gmail.com>
2022-04-12 11:14:48 +0300

    Completed Asiignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(source, sought) {
+  return source.filter(
+    (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+  )
+} 
+
+function fuzzyMatch (source, namesString) {
+  return source.filter(
+    (possibleMatch) => 
+    possibleMatch.toLowerCase().indexOf(namesString.toLowerCase()) === 0
+  );
+}
+
+function matchName (source, soughtName) {
+  return source.filter ((record) => record.name === soughtName);
+};
